1ec8df96b9a57fbaae550000471ab0892726f05d
JOEYLEE1121 <joeylee1121@gmail.com>
2022-11-16 15:52:53 +0800

    [Fix] Resolved merge error
    issue #7

---
+++ backend/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+from .database import SessionLocal, engine
+from . import models, schemas
+from .routers import users, auth
+
+models.Base.metadata.create_all(bind=engine)
+
+
+app = FastAPI()
+
+app.include_router(users.router)
+app.include_router(auth.router)
+
+@app.get("/")
+async def root():
+    return {"message": "hello to the root page"}
