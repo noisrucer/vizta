7226842fed2a027b6af0fa2c8bb51278ae49bcfa
Jason Lee <changjin9792@gmail.com>
2022-12-03 19:36:27 +0800

    [Fix] Fixed error for "major" column type in schemas.py
    issue #3

--- backend/schemas.py
+++ backend/schemas.py
@@ -15,7 +15,7 @@ class Major(str, Enum):
 class UserBase(BaseModel):
     email: str
     enteredYear: str
-    major: Major
+    major: str
     
     @validator('email')
     def email_must_be_valid(cls, v):
