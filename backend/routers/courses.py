2a23479249cedec54b555d60ce3a4124e5834fd7
Jason Lee <changjin9792@gmail.com>
2022-12-23 17:25:16 +0900

    [Style] Minor changes in /routers/courses.py
    issue #16

--- backend/routers/courses.py
+++ backend/routers/courses.py
@@ -9,7 +9,6 @@ router = APIRouter(
     tags=["courses"]
 )
 
-@router.get('/{faculty}', response_model=list[schemas.User])
+@router.get('/{faculty}')
 async def get_users(db: Session=Depends(get_db), skip: int = 0, limit: int = 100):
-    users = db.query(models.User).offset(skip).limit(limit).all()
-    return users
+    pass
