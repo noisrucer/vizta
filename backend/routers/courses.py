815f70cab6c73be67a36530484b3d91c5c802e6e
EMCJava <loyusum246807952@gmail.com>
2022-12-25 03:57:22 +0800

    [Feat] Completed getting static data for course review
    - Number of stats namely
    - GPA, LectureDifficulty, FinalDifficulty,
    - Workload, Entertaining, Delivery and Interactivity
    issue #20

--- backend/routers/courses.py
+++ backend/routers/courses.py
@@ -1,22 +1,25 @@
+from collections import Counter
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from ..database import get_db
 from .. import models, schemas
+from ..models import GPA, NumericEval
 
 router = APIRouter(
     prefix="/courses",
     tags=["courses"]
 )
 
-@router.get('/{faculty}', response_model = list[schemas.MainPageCourseOut])
-async def get_users(faculty: schemas.Faculty, db: Session=Depends(get_db)):
+
+@router.get('/{faculty}', response_model=list[schemas.MainPageCourseOut])
+async def get_courses(faculty: schemas.Faculty, db: Session = Depends(get_db)):
     # If faculty is all
     if faculty == 'All':
         courses = db.query(models.Course).all()
-    else: # If faculty is a specific faculty
+    else:  # If faculty is a specific faculty
         courses = db.query(models.Course).filter(models.Course.faculty == faculty).all()
-        
+
     response = []
     for c in courses:
         cid = c.course_id
@@ -28,5 +31,38 @@ async def get_users(faculty: schemas.Faculty, db: Session=Depends(get_db)):
             "name": cname,
             "num_reviews": num_reviews
         })
-        
+
     return response
+
+
+@router.get('/review/{course_id}', response_model=schemas.ReviewOut)
+async def get_review(course_id: str, db: Session = Depends(get_db)):
+    # If faculty is all
+    reviews = db.query(models.CourseReview).filter(models.CourseReview.course_id == course_id).all()
+
+    # TODO: 6. Overall - pentagon with each edge representing the average of each criteria. Missing?
+    # TODO: 7. (top of page) other info (final exam, midterm, assignments, project, ). Since it may vary by year or semester, show only the newest course(subclass) info?
+
+    def CountEnum(values, enumTy):
+        # uncomment this instead if zero values not needed
+        # return Counter(values)
+
+        return {k.value: 0 for k in enumTy} | Counter(values)
+
+    return {
+        "GPA": CountEnum([_.gpa for _ in reviews], GPA),
+        "LectureDifficulty": CountEnum([_.lecture_difficulty for _ in reviews], NumericEval),
+        "FinalDifficulty": CountEnum([_.final_exam_difficulty for _ in reviews], NumericEval),
+        "Workload": CountEnum([_.workload for _ in reviews], NumericEval),
+        "TeachingQuality": {
+            "Entertaining": CountEnum([_.course_entertaining for _ in reviews], NumericEval),
+            "Delivery": CountEnum([_.course_delivery for _ in reviews], NumericEval),
+            "Interactivity": CountEnum([_.course_interactivity for _ in reviews], NumericEval)
+        },
+        # "CourseInfo": {
+        #     "ExamRatio": 1,
+        #     "Midterm": 1,
+        #     "Assignments": 1,
+        #     "Project": 1,
+        # },
+    }
--- backend/schemas.py
+++ backend/schemas.py
@@ -1,22 +1,26 @@
-from typing import Union
+from typing import Union, List, Mapping
 from enum import Enum
 
 from email_validator import validate_email, EmailNotValidError
 from fastapi import HTTPException, status
 from pydantic import BaseModel, EmailStr, validator
 
+from backend.models import GPA, NumericEval
+
+
 class Major(str, Enum):
     comp_sci = "Computer Science"
     comp_eng = "Computer Engineering"
     info_sys = "Information System"
     other = "Other"
-    
+
+
 # Users
 class UserBase(BaseModel):
     email: str
     enteredYear: str
     major: Major
-    
+
     @validator('email')
     def email_must_be_valid(cls, v):
         try:
@@ -28,24 +32,27 @@ class UserBase(BaseModel):
                 detail="You must provide a valid email address type (ex. xxx@gmail.com)"
             )
         return email
-        
-    
+
+
 class UserCreate(UserBase):
     password: str
-    
+
+
 class UserCreateOut(UserBase):
     user_id: int
-    
+
     class Config:
         orm_mode = True
-    
+
+
 class User(UserBase):
     class Config:
         orm_mode = True
-        
+
+
 class VerifyEmail(BaseModel):
     email: str
-    
+
     @validator('email')
     def email_must_be_valid(cls, v):
         try:
@@ -57,37 +64,52 @@ class VerifyEmail(BaseModel):
                 detail="You must provide a valid email address type (ex. xxx@gmail.com)"
             )
         return email
-    
+
 
 # Email
 class EmailSchema(BaseModel):
-    email: list[EmailStr]
-    
-    
+    email: List[EmailStr]
+
+
 # JWT
 class Token(BaseModel):
     access_token: str
     token_type: str
 
+
 class TokenData(BaseModel):
     username: Union[str, None] = None
-    
-    
+
+
 # Course
 class CourseBase(BaseModel):
     course_id: str
     name: str
-    
-    
+
+
 class MainPageCourseOut(CourseBase):
     num_reviews: int
-    
+
     class Config:
         orm_mode = True
-        
+
+
+class ReviewOut(BaseModel):
+    GPA: Mapping[GPA, int]
+    LectureDifficulty: Mapping[NumericEval, int]
+    FinalDifficulty: Mapping[NumericEval, int]
+    Workload: Mapping[NumericEval, int]
+    TeachingQuality: Mapping[str, Mapping[NumericEval, int]]
+
+
 class Faculty(str, Enum):
     all = "All"
     beng = "BEng"
     bba = "BBA"
     bsc = "BSC"
     cc = "CC"
+
+
+class Semester(int, Enum):
+    first = 1
+    second = 2
