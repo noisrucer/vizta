0f550538875a8aead23e72b6c94367f859bd34f7
JOEYLEE1121 <joeylee1121@gmail.com>
2022-12-27 18:58:26 +0100

    [Bug] Fix sidebar profile
    issue #13

--- frontend/src/pages/Auth/SignIn.js
+++ frontend/src/pages/Auth/SignIn.js
@@ -43,11 +43,9 @@ const SignIn = () => {
   const [errorMessage, setErrorMessage] = useState("")
   const [openErrorMessage, setOpenErrorMessage] = useState(false);
 
-  const {UserData, UserToken, IsLoggedIn} = useContext(UserContext);
+  const {UserData, UserToken} = useContext(UserContext);
   const [userToken, setUserToken] = UserToken;
   const [userData, setUserData] = UserData;
-  const [isLoggedIn, setIsLoggedIn] = IsLoggedIn;
-
 
   const handleSetUserData = (e) => {
     setUserData(e);
@@ -87,7 +85,6 @@ const SignIn = () => {
           })
           .then(response => {
             console.log("response in Sign In: ",response)
-            setIsLoggedIn(true)
           })
           navigate('/main')
         }
