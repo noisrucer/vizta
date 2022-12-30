3b900b47cffaa19d55f0bd19a170efa92abcf85d
Jason Lee <changjin9792@gmail.com>
2022-12-30 14:19:59 +0900

    [Refact] Added more validations on `courses` schemas
    issue #32

--- backend/src/courses/schemas.py
+++ backend/src/courses/schemas.py
@@ -1,30 +1,34 @@
-from pydantic import BaseModel, EmailStr
-from typing import Union
+from pydantic import BaseModel, Field, EmailStr
 
 import backend.src.courses.enums as enums
+import backend.src.utils as glob_utils 
+
 
 class CourseBase(BaseModel):
-    course_id: str
+    course_id: str = Field(..., regex="^[a-zA-Z]{4}[0-9]{4}$")
     name: str
+
     
 class MainPageCourseOut(CourseBase):
-    num_reviews: int
+    num_reviews: int = Field(..., ge=0)
     class Config:
         orm_mode = True
     
     
 class UserFavoriteBase(BaseModel):
     email: EmailStr
-    course_id: str
+    course_id: str = Field(..., regex="^[A-Z]{4}[0-9]{4}$")
         
 
 class UserFavorite(UserFavoriteBase):
     class Config:
         orm_mode = True
+
         
 class UserFavoriteCreate(UserFavoriteBase):
     class Config:
         orm_mode = True
+
 
 class UserFavoriteCreateOut(UserFavoriteBase):
     class Config:
@@ -33,10 +37,10 @@ class UserFavoriteCreateOut(UserFavoriteBase):
 
 class UserReviewBase(BaseModel):
     email: EmailStr
-    course_id: str
-    subclass_id: str
-    academic_year: int
-    semester: int
+    course_id: str = Field(..., regex="^[A-Z]{4}[0-9]{4}$")
+    subclass_id: str = Field(..., regex="^[A-Z]{1}$")
+    academic_year: int = Field(..., ge=2000, le=glob_utils.get_current_year())
+    semester: int = Field(..., ge=1, le=2)
     gpa: enums.GPA
     workload: enums.NumericEval
     lecture_difficulty: enums.NumericEval
@@ -44,13 +48,15 @@ class UserReviewBase(BaseModel):
     course_entertaining: enums.NumericEval
     course_delivery: enums.NumericEval
     course_interactivity: enums.NumericEval
-    final_exam_ratio: int
-    midterm_ratio: int
-    assignments_ratio: int
-    project_ratio: int
+    final_exam_ratio: int = Field(..., ge=0, le=100)
+    midterm_ratio: int = Field(..., ge=0, le=100)
+    assignments_ratio: int = Field(..., ge=0, le=100)
+    project_ratio: int = Field(..., ge=0, le=100)
+
 
 class UserReviewCreateIn(UserReviewBase):
     pass
+
 
 class UserReviewCreateOut(UserReviewBase):
     review_id: int
