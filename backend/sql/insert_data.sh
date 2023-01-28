ad3fa8b062d784c6adb55020222c2ce97efd85f5
Jason Lee <changjin9792@gmail.com>
2023-01-28 15:57:29 +0800

    [Extra] Shell script to insert data
    issue #73

---
+++ backend/sql/insert_data.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+host="localhost"
+username="root"
+dbname="vizta"
+password=""
+declare -a filenames=(
+    "courses.sql" "users.sql" "prerequisites.sql" "mutual_exclusives.sql" "blocking_course.sql" "course_allowed_year.sql" "reviews.sql"
+)
+
+mysql -h $host -u $username -p$password $dbname --execute="drop database ${dbname}; create database ${dbname}; use ${dbname};"
+echo "Dropped ${dbname}"
+echo "Created ${dbname}"
+echo "Using ${dbname}..."
+
+uvicorn backend.src.main:app --reload
+
+for fname in "${filenames[@]}"; do
+    mysql -h $host -u $username -p$password $dbname < "backend/sql/${fname}" 2> /dev/null
+    echo "Successfully executed ${fname}"
+done
+
