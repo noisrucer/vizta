4a4607635a9efddd5d1659e936462d3a873d9cbb
EMCJava <loyusum246807952@gmail.com>
2023-01-29 16:05:47 +0800

    [Feat] Added more tmp review data
    issue #20

--- backend/sql/reviews.sql
+++ backend/sql/reviews.sql
@@ -1,18 +1,14 @@
 -- Active: 1671800850866@@127.0.0.1@3306@vizta
 INSERT INTO course_review
-VALUES (DEFAULT, 'loyusum246807952@gmail.com', 'A', 'COMP2396', 2022, 1, 'A+', 7, 4, 4, 6, 6, 5, 60, 10, 30, 0),
-       (DEFAULT, 'loyusum@gmail.com', 'A', 'COMP2396', 2022, 1, 'F', 1, 1, 1, 1, 1, 1, 60, 20, 20, 0),
-       (DEFAULT, 'loyusum@gmail.com', 'B', 'COMP2396', 2022, 2, 'A+', 7, 4, 4, 6, 6, 5, 60, 10, 30, 0),
-       (DEFAULT, 'loyusum246807952@gmail.com', 'B', 'COMP2396', 2022, 2, 'A+', 2, 2, 1, 1, 1, 1, 50, 20, 30, 0),
-       (DEFAULT, 'loyusum246807952@gmail.com', 'B', 'COMP2396', 2022, 1, 'D', 10, 10, 10, 2, 3, 2, 60, 10, 30, 0),
-       (DEFAULT, 'loyusum246807952@gmail.com', 'A', 'COMP2396', 2022, 2, 'F', 7, 4, 4, 6, 6, 5, 60, 10, 30, 0);
-
-INSERT INTO course_review
-VALUES (DEFAULT, 'loyusum246807952@gmail.com', 'A', 'COMP3314', 2022, 1, 'A+', 7, 4, 4, 6, 6, 5, 60, 10, 30, 0),
-       (DEFAULT, 'loyusum@gmail.com', 'A', 'COMP3314', 2022, 1, 'F', 1, 1, 1, 1, 1, 1, 60, 20, 20, 0),
-       (DEFAULT, 'loyusum@gmail.com', 'A', 'COMP3314', 2023, 1, 'C', 1, 10, 1, 10, 1, 10, 60, 20, 20, 0),
-       (DEFAULT, 'loyusum@gmail.com', 'B', 'COMP3314', 2022, 2, 'A+', 7, 4, 4, 6, 6, 5, 60, 10, 30, 0),
-       (DEFAULT, 'loyusum246807952@gmail.com', 'C', 'COMP3314', 2022, 2, 'A+', 2, 2, 1, 1, 1, 1, 50, 20, 30, 0),
-       (DEFAULT, 'loyusum246807952@gmail.com', 'B', 'COMP3314', 2022, 2, 'D', 10, 10, 10, 2, 3, 2, 60, 10, 30, 0),
-       (DEFAULT, 'loyusum@gmail.com', 'C', 'COMP3314', 2022, 2, 'D', 1, 1, 1, 1, 1, 1, 60, 10, 30, 0),
-       (DEFAULT, 'loyusum246807952@gmail.com', 'A', 'COMP3314', 2022, 1, 'F', 7, 4, 4, 6, 6, 5, 60, 10, 30, 0);
+VALUES (DEFAULT, 'loyusum246807952@gmail.com', 'A', 'ADEB3340', 2022, 1, 'A+', 3, 4, 4, 4, 4, 5, 60, 10, 30, 0),
+       (DEFAULT, 'loyusum246807952@gmail.com', 'A', 'ADEB3340', 2022, 1, 'F', 1, 1, 1, 1, 1, 1, 60, 20, 20, 0),
+       (DEFAULT, 'loyusum246807952@gmail.com', 'A', 'ADEB3340', 2022, 1, 'F', 3, 4, 4, 4, 4, 5, 60, 20, 20, 0),
+       (DEFAULT, 'loyusum246807952@gmail.com', 'B', 'ADEB3340', 2022, 1, 'D+', 3, 4, 4, 4, 4, 5, 60, 20, 20, 0),
+       (DEFAULT, 'loyusum246807952@gmail.com', 'B', 'ADEB3340', 2022, 1, 'A+', 2, 2, 1, 1, 1, 1, 50, 20, 30, 0),
+       (DEFAULT, 'loyusum246807952@gmail.com', 'B', 'ADEB3340', 2022, 1, 'D', 5, 5, 5, 2, 3, 2, 60, 10, 30, 0),
+       (DEFAULT, 'loyusum246807952@gmail.com', 'B', 'ADEB3340', 2023, 2, 'C+', 3, 4, 4, 4, 4, 5, 60, 10, 30, 0),
+       (DEFAULT, 'loyusum246807952@gmail.com', 'B', 'ADEB3340', 2023, 2, 'F', 1, 1, 1, 1, 1, 1, 60, 20, 20, 0),
+       (DEFAULT, 'loyusum246807952@gmail.com', 'B', 'ADEB3340', 2023, 2, 'F', 3, 4, 4, 4, 4, 5, 60, 10, 30, 0),
+       (DEFAULT, 'loyusum246807952@gmail.com', 'A', 'ADEB3340', 2023, 2, 'B+', 3, 4, 4, 4, 4, 5, 60, 10, 30, 0),
+       (DEFAULT, 'loyusum246807952@gmail.com', 'A', 'ADEB3340', 2023, 2, 'A+', 2, 2, 1, 1, 1, 1, 50, 20, 30, 0),
+       (DEFAULT, 'loyusum246807952@gmail.com', 'A', 'ADEB3340', 2023, 2, 'D', 4, 1, 5, 2, 3, 2, 60, 10, 30, 0);
