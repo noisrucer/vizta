f2032e6956d0c33817a234fc51bfe03a3c245bc5
Jason Lee <changjin9792@gmail.com>
2023-02-05 23:53:19 +0800

    landing page complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { Route, Routes } from "react-router-dom";
 import Landing from "./pages/Landing/Landing";
@@ -8,55 +9,63 @@ import Main from "./pages/Main/Main";
 import { UserProvider } from "./UserContext";
 import Sidebar from "./components/Sidebar/Sidebar";
 // import RouteError from "./RouteError";
+import { useState, useContext } from "react";
+import { UserContext } from "./UserContext";
 import ProtectedRoutes from "./ProtectedRoutes";
 import Profile from "./components/Sidebar/Profile";
 import Visualization from "./pages/Visualization/Visualization";
-import { amber, deepOrange, grey } from '@mui/material/colors';
+import { amber, deepOrange, grey } from "@mui/material/colors";
 
-const temp_color = "#1D2630"
+const temp_color = "#1D2630";
+// console.log("UC", UserContext);
 
 const darkTheme = createTheme({
   palette: {
     mode: "dark",
     primary: {
-      main: amber[500]
+      main: amber[500],
     },
     secondary: {
-      main: deepOrange[500]
+      main: deepOrange[500],
     },
     background: {
-      default: temp_color
+      default: temp_color,
     },
     action: {
       disabled: "lightgrey",
-    }
+    },
   },
 });
 
 const App = () => {
+  const { IsLanding } = React.useContext(UserContext);
+  const [isLanding, setIsLanding] = IsLanding;
+
   return (
-    <ThemeProvider theme={darkTheme}>
-      <UserProvider>
-        <Sidebar />
-        <Routes>
-          <Route path="/" element={<Landing />}></Route>
-          <Route path="/auth/sign-in" element={<SignIn />}></Route>
-          <Route path="/auth/sign-up" element={<SignUp />}></Route>
-          <Route path="/auth/reset-password" element={<ResetPassword />}></Route>
-          <Route element={<ProtectedRoutes />}>
-            <Route path="/auth/me"></Route>
-            <Route path="/main">
-              <Route path=":faculty" element={<Main />}></Route>
-            </Route>
-            <Route path="/visualization" >
-              <Route path=":courseId" element={<Visualization />}></Route>
-            </Route>
-            <Route path="/profile" element={<Profile />}></Route>
+    // <ThemeProvider theme={darkTheme}>
+    // <UserProvider>
+    <>
+      {isLanding ? <></> : <Sidebar />}
+      <Routes>
+        <Route path="/" element={<Landing />}></Route>
+        <Route path="/auth/sign-in" element={<SignIn />}></Route>
+        <Route path="/auth/sign-up" element={<SignUp />}></Route>
+        <Route path="/auth/reset-password" element={<ResetPassword />}></Route>
+        <Route element={<ProtectedRoutes />}>
+          <Route path="/auth/me"></Route>
+          <Route path="/main">
+            <Route path=":faculty" element={<Main />}></Route>
           </Route>
-        </Routes>
-      </UserProvider>
-      {/* <RouteError /> */}
-    </ThemeProvider>
+          <Route path="/visualization">
+            <Route path=":courseId" element={<Visualization />}></Route>
+          </Route>
+          <Route path="/profile" element={<Profile />}></Route>
+        </Route>
+      </Routes>
+    </>
+    // </UserProvider>
+
+    // </ThemeProvider>
   );
 };
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,40 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { UserProvider } from "./UserContext";
+import { amber, deepOrange, grey } from "@mui/material/colors";
+
 import "./index.css";
+
+const temp_color = "#1D2630";
+// console.log("UC", UserContext);
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: amber[500],
+    },
+    secondary: {
+      main: deepOrange[500],
+    },
+    background: {
+      default: temp_color,
+    },
+    action: {
+      disabled: "lightgrey",
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </ThemeProvider>
   </BrowserRouter>
 );
