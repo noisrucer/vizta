259bc5c002747d71a5d7626fbcc641a20e473f68
Yonghyun Kwon <yonghyunkwon98@gmail.com>
2023-03-01 11:50:29 +0800

    Deleted frontend

--- frontend/src/pages/Auth/ErrorMessage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ErrorMessage = () => {
-  return (
-    <div>ErrorMessage</div>
-  )
-}
-
-export default ErrorMessage
--- frontend/src/pages/Landing/Pricing.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "../../styles/main.css";
-const Pricing = () => {
-  return <br />;
-};
-
-export default Pricing;
