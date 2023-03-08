6631c616cf37b402b24f1757dce68a8c49318880
JOEYLEE1121 <joeylee1121@gmail.com>
2023-03-08 14:01:50 +0800

    [Feat] Button render set-up

--- frontend/src/pages/Visualization/Tabs/YearlyTrend.jsx
+++ frontend/src/pages/Visualization/Tabs/YearlyTrend.jsx
@@ -1,3 +1,7 @@
+import { useState, useEffect, useContext } from "react";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Switch from "@mui/material/Switch";
 import Box from "@mui/material/Box";
 import { useTheme } from "@mui/material";
 import { ColorModeContext, tokens } from "../../../theme";
@@ -7,6 +11,55 @@ import lineData from "./NivoData/LineData";
 const YearlyTrend = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
+
+  const [chartData, setChartData] = useState({
+    labels: [],
+    datasets: [],
+  });
+
+  const initialState = chartData.datasets.reduce((acc, dataset) => {
+    acc[dataset.label] = false;
+    return acc;
+  }, {});
+
+  initialState[Object.keys(initialState)[0]] = true;
+  initialState[Object.keys(initialState)[1]] = true;
+
+  const [state, setState] = useState(initialState);
+  const [switchClicked, setSwitchClicked] = useState(false);
+
+  useEffect(() => {
+    setState(initialState);
+  }, [chartData]);
+
+  function renderSwitch(prof) {
+    const handleChange = (event) => {
+      setSwitchClicked(true);
+      setState({
+        ...state,
+        [event.target.name]: !state[event.target.name],
+      });
+    };
+
+    const label = { inputProps: { "aria-label": "Switch demo" } };
+
+    return (
+      <FormGroup>
+        <FormControlLabel
+          sx={{ width: "250px" }}
+          control={
+            <Switch
+              {...label}
+              checked={state[prof.label]}
+              onClick={handleChange}
+              name={prof.label}
+            />
+          }
+          label={prof.label}
+        />
+      </FormGroup>
+    );
+  }
   return (
     <Box
       display="grid"
@@ -25,11 +78,13 @@ const YearlyTrend = () => {
         flexDirection="row"
         borderRadius="2%"
       >
-        <Box sx={{ width: "80%", height: "500px" }}>
+        <Box sx={{ width: "80%", height: "555px" }}>
           <NivoLineChart data={lineData} />
         </Box>
         <Box width="20%">
-
+          {chartData.datasets.map((item) => {
+            return renderSwitch(item);
+          })}
         </Box>
       </Box>
     </Box>
